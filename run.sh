fa91fb484819e8b07608f6cc930a003316b8cf49
zejunwang <zejunwang@pku.edu.cn>
2021-12-13 16:36:32 +0800

    shell update

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
-CUDA_VISIBLE_DEVICES=0 python train_AI.py \
+#!/bin/bash
+
+GpU=$1
+CUDA_VISIBLE_DEVICES="$GPU" python train_AI.py \
                         --model_type CNN \
                         --embedding_type emd \
                              
